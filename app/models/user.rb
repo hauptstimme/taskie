1d368b164e43f3940a7ce701974149d19d90661f
Alexander Skiba <skiba-alex@ya.ru>
2013-10-03 09:36:56 +0400

    Add Devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,5 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 end
