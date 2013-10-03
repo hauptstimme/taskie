89077c36bc7b8c8d53936d9c58be5fb39577157b
Alexander Skiba <skiba-alex@ya.ru>
2013-10-03 18:00:37 +0400

    Add a checkbox near the task description that toggles completion status

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,58 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
   before_action :set_users, only: [ :new, :edit ]
+  before_action :set_task, only: [ :show, :edit, :update, :destroy ]
 
-  scaffold! permit_fields: [ :assignee_id, :name, :details, :status ]
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to @task, notice: 'Task was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    if request.patch?
+      @success = @task.update(task_params)
+    else
+      if @task.update(task_params)
+        redirect_to @task, notice: 'Task was successfully updated.'
+      else
+        render action: 'edit'
+      end
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_url, notice: 'Task was successfully destroyed.'
+  end
 
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit([:assignee_id, :name, :details, :status])
+  end
 
   def set_users
     @users ||= User.all.map{ |n| [ n.email, n.id ] }
