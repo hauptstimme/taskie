cfbb8e48e449ddc4d4f07544949fcdb99ee330ea
Alexander Skiba <skiba-alex@ya.ru>
2013-10-04 13:25:38 +0400

    Add Gravatar link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,10 @@ module ApplicationHelper
   def gravatar_for user, options = {}
     hash = Digest::MD5.hexdigest(user.email)
     size = options[:size] || 20
-    image_tag("https://gravatar.com/avatar/#{hash}?s=#{size}", id: "gravatar")
+    unless user_signed_in?
+      image_tag("https://gravatar.com/avatar/#{hash}?s=#{size}", id: "gravatar")
+    else
+      link_to image_tag("https://gravatar.com/avatar/#{hash}?s=#{size}", id: "gravatar"), "http://gravatar.com", target: "_blank"
+    end
   end
 end
