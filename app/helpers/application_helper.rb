f5fa58beb018f9100200e952cff025d3234d8a5e
Alexander Skiba <skiba-alex@ya.ru>
2013-10-04 15:43:44 +0400

    Display Gravatar link only for the current user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def gravatar_for user, options = {}
     hash = Digest::MD5.hexdigest(user.email)
     size = options[:size] || 20
-    unless user_signed_in?
+    unless user_signed_in? and current_user == user
       image_tag("https://gravatar.com/avatar/#{hash}?s=#{size}", id: "gravatar")
     else
       link_to image_tag("https://gravatar.com/avatar/#{hash}?s=#{size}", id: "gravatar"), "http://gravatar.com", target: "_blank"
