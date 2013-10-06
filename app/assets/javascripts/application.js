06bb73cf4476c845816373316850f86497c5a357
Alexander Skiba <skiba-alex@ya.ru>
2013-10-06 16:25:50 +0400

    Show spinner when turbolinks are fetching

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,12 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require bootstrap
+
+$(document).ready(function(){
+  document.addEventListener("page:fetch", function(){
+    $("#loader").show();
+  });
+  document.addEventListener("page:receive", function(){
+    $("#loader").hide();
+  });
+});
