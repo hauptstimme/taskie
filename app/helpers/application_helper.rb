debcae3cf492e382665114e90c8640af6f03df3f
Alexander Skiba <skiba-alex@ya.ru>
2013-11-04 03:30:50 +0400

    Refactor gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def gravatar_for user, options = {}
-    hash = Digest::MD5.hexdigest(user.email)
     size = options[:size] || 20
-    unless user_signed_in? and current_user == user
-      image_tag("https://gravatar.com/avatar/#{hash}?s=#{size}", id: "gravatar")
+    image = image_tag("https://gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}?s=#{size}", id: "gravatar", style: "width: #{size}px")
+    unless user_signed_in? and current_user.id == user.id
+      image
     else
-      link_to image_tag("https://gravatar.com/avatar/#{hash}?s=#{size}", id: "gravatar"), "http://gravatar.com", target: "_blank"
+      link_to image, "http://gravatar.com", target: "_blank"
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Taskie::Application.routes.draw do
   concern :commentable do
-    resources :comments, only: [ :show, :create, :edit, :update, :destroy ]
+    resources :comments, only: [:show, :create, :edit, :update, :destroy]
   end
 
   devise_for :users
+
   resources :projects do
     resources :tasks, concerns: :commentable
   end
