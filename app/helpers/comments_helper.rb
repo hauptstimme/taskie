e2c2cba991a4b85198a1ad6e0a4697a5e69443b0
Alexander Skiba <lacrosse@maldoror.tk>
2013-11-21 20:31:44 +0400

    Refactor user_with_gravatar

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -3,11 +3,6 @@ module CommentsHelper
     content_tag :span, text, class: "has-tooltip", data: { title: tooltip }
   end
 
-  # TODO
-  def activity_details_with_tooltip text, tooltip
-    span_with_tooltip text, tooltip
-  end
-
   def time_distance_with_tooltip time
     span_with_tooltip distance_of_time_in_words(time, Time.now), time.to_s(:long)
   end
